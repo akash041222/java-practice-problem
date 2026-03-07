67082bb7e6e28fb2d07ddbde691827246dd61a21
akash041222 <akash.041222@gmail.com>
2026-03-07 21:56:23 +0530

    Practised Array problems based on two pointers

--- array_based_problems/src/TwoSumSortedArray.java
+++ array_based_problems/src/TwoSumSortedArray.java
@@ -3,7 +3,8 @@ public class TwoSumSortedArray {
     public static void main(String[] args){
         int[] arr = {1,2,3,4,6};
         int target=7;
-        int left=0,right=arr.length-1;
+        int left=0;
+        int right=arr.length-1;
         while(left<right){
             int sum=arr[left]+arr[right];
             if(sum==target){
